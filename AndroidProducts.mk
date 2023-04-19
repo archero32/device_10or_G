ff338e4d45ebc943910c9edeaa8e748e327619ea
Lalit <123730669+archero32@users.noreply.github.com>
2023-04-19 20:03:22 +0530

    Bananadroid Bringup

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/banana_G.mk
+
+COMMON_LUNCH_CHOICES := \
+    banana_G-user \
+    banana_G-userdebug \
+    banana_G-eng
